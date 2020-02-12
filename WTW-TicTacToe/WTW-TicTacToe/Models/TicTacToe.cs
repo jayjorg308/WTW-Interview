047ffc1bbbda97db8c8af814117747880f22643e
Jayson Jorgensen <jay.jorg308@gmail.com>
2020-02-11 20:55:21 -0700

    Initial Tic-Tac-Toe commit

---
+++ WTW-TicTacToe/WTW-TicTacToe/Models/TicTacToe.cs
@@ -0,0 +1,145 @@
+﻿using System;
+
+namespace WTW_TicTacToe.Models
+{
+    public class TicTacToe
+    {
+        // Private variables for use in TicTacToe class
+        private char[,] _board;
+        private int _boardSize;
+
+        /// <summary>
+        /// Created a Tic Tac Tow game board
+        /// </summary>
+        /// <param name="n">nxn dimension for the game board</param>
+        public TicTacToe(int n)
+        {
+            // initialize our nxn board
+            _board = new char[n, n];
+
+            // set our boardSize variable for use in checking for winner
+            _boardSize = n;
+        }
+
+        /// <summary>
+        /// Place a piece on the game board
+        /// </summary>
+        /// <param name="row">row to place a piece</param>
+        /// <param name="col">column to place a piece</param>
+        /// <param name="player">the player (1 or 2) the piece is for</param>
+        /// <returns>0 = no winner, 1 = player 1 won, 2 = player 2 won</returns>
+        public int PlacePiece(int row, int col, int player)
+        {
+            // storing the current player's character and setting the square with the character
+            var character = player == 1 ? 'X' : 'O';
+            _board[row, col] = character;
+
+            // calling private method to display tic tac toe board in console
+            DisplayBoard();
+
+            // calling private "CheckForWinner" method to see if the game is over
+            // if CheckForWinner returns true, our method returns the winning player's number
+            // else, returns 0 and the game keeps going
+            return CheckForWinner(character) ? player : 0;
+        }
+
+        #region Private Methods
+        // private method that will check all of the private methods below for a winner.
+        // if any of the methods return true, we will return true
+        // if not, we will return false, and the game will continue
+        private bool CheckForWinner(char character)
+        {
+            if (HorizontalWinner(character))
+                return true;
+            else if (VerticalWinner(character))
+                return true;
+            else if (DiagonalWinner(character))
+                return true;
+            else
+                return false;
+        }
+
+        // private method for checking for a diagonal winner
+        private bool DiagonalWinner(char character)
+        {
+            // check first diagonal
+            for (int x = 0; x < _boardSize; x++)
+            {
+                if (_board[x, x] != character)
+                    break;
+
+                if (x == (_boardSize - 1))
+                    return true;
+            }
+
+            // check second diagonal
+            for (int row = 0, col = _boardSize - 1; row < _boardSize; row++, col--)
+            {
+                if (_board[row, col] != character)
+                    break;
+
+                if (row == (_boardSize - 1))
+                    return true;
+            }
+
+            return false;
+        }
+
+        // private method for checking vertical columns for a winner
+        private bool VerticalWinner(char character)
+        {
+            for (int col = 0; col < _boardSize; col++)
+            {
+                var colSum = 0;
+                for (int row = 0; row < _boardSize; row++)
+                {
+                    if (_board[row, col] == character)
+                        colSum++;
+                    else
+                        break;
+                }
+                if (colSum == _boardSize)
+                    return true;
+            }
+
+            return false;
+        }
+
+        // private method for checking horizontal rows for a winner
+        private bool HorizontalWinner(char character)
+        {
+            for (int row = 0; row < _boardSize; row++)
+            {
+                var rowSum = 0;
+                for (int col = 0; col < _boardSize; col++)
+                {
+                    if (_board[row, col] == character)
+                        rowSum++;
+                    else
+                        break;
+                }
+                if (rowSum == _boardSize)
+                    return true;
+            }
+
+            return false;
+        }
+
+        // private method for creating a console representation of our tic tac toe board
+        private void DisplayBoard()
+        {
+            Console.WriteLine();
+            for (int row = 0; row < _boardSize; row++)
+            {
+                Console.Write("| ");
+                for (int col = 0; col < _boardSize; col++)
+                {
+                    Console.Write(_board[row, col]);
+                    Console.Write(" | ");
+                }
+                Console.WriteLine();
+            }
+        }
+        #endregion
+    }
+}
---
+++ WTW-TicTacToe/WTW-TicTacToe/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using WTW_TicTacToe.Models;
+
+namespace WTW_TicTacToe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Prompt user to enter a single number that will represent the nxn tic tac toe board
+            Console.WriteLine("Please enter a single number 'n' that will represent your nxn sized tic tac toe board: ");
+            var boardSize = Convert.ToInt32(Console.ReadLine());
+
+            // instantiate a new tic tac toe board using the user input
+            var board = new TicTacToe(boardSize);
+
+            // properties for keeping track of game
+            var result = 0;
+            var player1 = true;
+            var totalMoves = 0;
+
+            // do while loop that will run while result is 0 (meaning no winner)
+            // AND while total moves is less than the board size
+            do
+            {
+                // Get the current player
+                var currentPlayer = player1 ? "Player 1" : "Player 2";
+
+                // prompt current player to enter two numbers representing the row and column repsectively
+                Console.WriteLine($"{currentPlayer}, please enter the zero-based row and column (separated by a space) you would like to play: ");
+                var response = Console.ReadLine().Split(' ');
+
+                // call the TicTacToe PlacePiece method
+                // this will return the result of the piece being placed
+                result = board.PlacePiece(Convert.ToInt32(response[0]), Convert.ToInt32(response[1]), player1 ? 1 : 2);
+
+                // if result is greater than 0, then we have a winner
+                if (result > 0)
+                    Console.WriteLine($"Player {result} wins!");
+
+                // increment the total number of moves
+                totalMoves++;
+
+                // check to see if the board is full
+                if (result == 0 && totalMoves == (boardSize * boardSize))
+                    Console.WriteLine("The game is a tie!");
+
+                // switch players
+                player1 = !player1;
+            }
+            while (result == 0 && totalMoves < boardSize * boardSize);
+        }
+    }
+}
